111215a8e1dbd25a962f9c9e2c83cab295d3321e
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-07-27 07:24:59 +0500

    fix: wire real routes into the entrypoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	"github.com/rs/zerolog/log"
+
+	"migrated-app/internal/resources"
 )
 
 func main() {
@@ -17,7 +19,7 @@ func main() {
 
 	srv := &http.Server{
 		Addr:    ":8080",
-		Handler: buildRouter(),
+		Handler: resources.BuildRouter(),
 	}
 
 	go func() {

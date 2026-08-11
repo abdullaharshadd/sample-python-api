246777a954cb1af829b91a3ea6de0be839d0e121
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-08-12 04:50:02 +0500

    fix: wire real routes into the entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { server } from './src/server/instance';
+import { registerRoutes } from './src/main';
 
+registerRoutes();
 server.run();
 
 export default server.app;

783153489ab3cd40055ded87512d58a255b128d8
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-08-12 17:40:20 +0500

    fix: server startup error — src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import 'dotenv/config';
 import { server } from './src/server/instance';
-import { environmentConfig } from './src/environment/instance';
 
-server.app.listen(environmentConfig.port, () => {
-  console.log(
-    `Server listening on port ${environmentConfig.port}${
-      environmentConfig.debug ? ' (debug)' : ''
-    }`
-  );
-});
+server.run();
 
 export default server.app;
